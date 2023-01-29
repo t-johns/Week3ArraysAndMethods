37807edf078565bd043deec2222b4187ac86075e
Trevor <trjohnson.1994@gmail.com>
2023-01-29 01:37:16 -0500

    Add weekly assignment checker via HashMap.

--- week3ArraysAndMethods/Week3CodingAssign.java
+++ week3ArraysAndMethods/Week3CodingAssign.java
@@ -1,18 +1,21 @@
 package week3ArraysAndMethods;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class Week3CodingAssign {
 
   public static void main(String[] args) {
     //1. Create an array of int called ages containing values 3, 9, 23, 64, 2, 8, 28, 93
     int[] ages= {3, 9, 23, 64, 2, 8, 28, 93, 84};
-    //programmatically subtract first element value, from the last, without using 'ages[7]'
+    //a. programmatically subtract first element value, from the last, without using 'ages[7]'
     int firstMinusLast = (ages[ages.length-1]) - ages[0];
-    //Add a new age (84) to ages and repeat previous step to ensure dynamic behavior
+    //b. Add a new age (84) to ages and repeat previous step to ensure dynamic behavior
     System.out.println(firstMinusLast);
 
-    //Use a loop to iterate through ages and calculate average, print result to console
+    //c. Use a loop to iterate through ages and calculate average, print result to console
     int ageSum = 0;
     for (int i=0; i<ages.length; i++) {
       ageSum += ages[i];
@@ -21,14 +24,14 @@ public class Week3CodingAssign {
         
     //2. Create an array of String with names Sam, Tommy, Sally, Buck, Bob
     String[] names = {"Sam", "Tommy", "Sally", "Buck", "Bob"};
-    //Use a loop to iterate through array and calculate avg number of letters per name, print result
+    //a. Use a loop to iterate through array and calculate avg number of letters per name, print result
     int totalLetters = 0;
     for (String name: names) {
       totalLetters += name.length();
     } int avgLetters = totalLetters / names.length;
     System.out.println("Avg letters: " + avgLetters);
     
-    //Use a loop to iterate names again, concat. all names together seperate by spaces
+    //b. Use a loop to iterate names again, concat. all names together seperate by spaces, print result
     int i = 0;
     String listedNames = "";
     while (i<names.length) {
@@ -36,9 +39,9 @@ public class Week3CodingAssign {
       i++; if (i == names.length) {
         break;
       } listedNames += ", ";
-    } System.out.println(listedNames);
+    } System.out.println("Names w/ spaces: " + listedNames);
       
-    //3. How to access last element of any array? 'array.length -1'
+    //3. How to access last element of any array? 'array.length - 1'
     //4. Access first element? array[0]
     //5. Create new array of nameLengths write loop to iterate prev. names array, add len of each to nameLengths
     int[] nameLengths = new int [names.length];
@@ -47,25 +50,25 @@ public class Week3CodingAssign {
       nameLengths[l] = names[l].length();
       //System.out.println(nameLengths[l]);
       l++;
-      } System.out.println(Arrays.toString(nameLengths));
+      } System.out.println("Name lengths: " + Arrays.toString(nameLengths));
 
     //6. Write loop to iterate nameLengths, calculate sum of all elements, print to console
     int nameLenSum = 0;
     for (int t = 0; t < nameLengths.length; t++) {
       nameLenSum += nameLengths[t];
-      } System.out.println(nameLenSum);
+      } System.out.println("Name lenth sum: " + nameLenSum);
       
     //7. Result- concattedWord
       //String word = "Hello";
       //int times = 3;
       String concatWord = concatStringMultInt("Hello", 3);
-      System.out.println(concatWord);
+      System.out.println("Concatted word: " + concatWord);
       
     //8. Result- fullName
       String firstName = "Ada";
       String lastName = "Lovelace";
       String fullName = makeFullName(firstName, lastName);
-      System.out.println(fullName);
+      System.out.println("Full name: " + fullName);
       
     //9. Result- is array of int > 100
       System.out.println("Is greater than 100: " + arrGreaterThanHundred(ages));
@@ -82,7 +85,24 @@ public class Week3CodingAssign {
       boolean isHotOutside = true;
       double moneyInPocket = 10.55;
       System.out.println("Will buy drink: " + willBuyDrink(isHotOutside, moneyInPocket));
-  }
+      
+    //13. Is week 4 complete? Worry no more with a boolean piece of mind to tell you if you have completed your assignments
+      //boolean week4IsComplete = false;
+      String watchedVids = "y";
+      String attendedClass = "y";
+      String openClassMastery = "y";
+      String codingAssignment = "n";
+      String researchAssignment = "n";
+      
+      isWeek4Complete(watchedVids, attendedClass, openClassMastery, codingAssignment, researchAssignment);
+      
+      
+
+        
+      }
+      
+      
+  
   //7. Write method that takes a String- word, and int- n. Method returns word*int, eg 'hi', 3="hihihi"
   private static String concatStringMultInt(String word, int times) {
     String concattedWord = "";
@@ -148,9 +168,9 @@ public class Week3CodingAssign {
     double arrAAvg = arrASum / arrA.length; //arr avgs
     double arrBAvg = arrBSum / arrB.length; //arr avgs
     return arrAAvg > arrBAvg;
-
     }
-
+  
+  //12. Write a method called willBuyDrink that takes boolean isHotOutside and double moneyInPocket. Return true if it is hot outside and moneyInPocket > 10.50
   private static boolean willBuyDrink(boolean isHotOutside, double moneyInPocket) {
     if (isHotOutside == true) {
       if (moneyInPocket > 10.50) {
@@ -158,4 +178,56 @@ public class Week3CodingAssign {
       }
     } return false;
   }
-}
+  //13. Create a method that solves a problem
+  // isWeek4Complete
+  public static HashMap<String, String> isWeek4Complete(String watchedVids, String attendedClass, String openClassMastery, String codingAssignment, String researchAssignment) {
+    HashMap<String, String> week4Assignments = new HashMap<String, String>(); // all assignments
+    HashMap<String, String> incomplete = new HashMap<String, String>(); //incomplete to track
+    
+    week4Assignments.put("Watched Videos", watchedVids); //Hashmap Assignment key/answer
+    week4Assignments.put("Attended Class", attendedClass);
+    week4Assignments.put("OpenClass Mastery", openClassMastery);
+    week4Assignments.put("Coding Assignment", codingAssignment);
+    week4Assignments.put("Research Assignment", researchAssignment);
+
+    //for (int i = 0; i < week4Assignments.size(); i++) {
+    //    System.out.println(week4Assignments.get("Watched Videos; ", watchedVids));
+    //    System.out.println(week4Assignments.values());
+    //}
+    for (String assignment: week4Assignments.keySet()) { // for assignment in week4Assignment keyset
+        
+        if (week4Assignments.get(assignment).equals("n")) { // if keySet.equals("n") ;          
+          incomplete.put(assignment, week4Assignments.get(assignment)); // add to incomplete
+        }
+        //System.out.print(assignment + ": "); // get assignment name
+        //System.out.println(week4Assignments.get(assignment)); // get value
+    }
+    if (incomplete.size() > 0) {      
+      System.out.println("I N C O M P L E T E: "); //incomplete warning
+      
+      for (String incompleteAssignment: incomplete.keySet()) {
+        System.out.println(incompleteAssignment);
+      }
+    }
+
+        //for (String assignment: week4Assignments()) {
+          
+     //   }
+    
+
+    
+    
+    //System.out.println(incomplete);
+    return incomplete;
+    //if (input.equals("n")
+    //for (boolean assignment: week4Complete) {
+    //  if (assignment == false) {
+    //    return false;
+    //  } else {
+    //    return true;
+     // }
+  }
+  
+  
+  }  
+//}
